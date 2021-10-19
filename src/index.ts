758ba829d654c9d11ba42dbc53af054d098cdea4
Darvesh <hello@darve.sh>
2021-10-19 20:05:50 +0530

    feat: add help and start commands

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
 import mongoose from "mongoose";
 
 import {
@@ -14,12 +14,19 @@ const bot = new Bot(BOT_TOKEN);
 
 const db = new Db(mongoose, MONGO_DB);
 
-bot.command("help", (ctx) =>
-  ctx.reply("This is an inline bot. Type @thewatbot word")
-);
-
-bot.command("start", (ctx) =>
-  ctx.reply("Developer: @solooo7. https://buymeacoffee.com/darvesh")
+["help", "start"].forEach((command) =>
+  bot.command(command, (ctx) =>
+    ctx.reply(
+      `An Inline English-to-English Dictionary Bot. \nTo use it, type <code>@thewatbot word</code> \nWritten by: @solooo7`,
+      {
+        parse_mode: "HTML",
+        webpage_preview: false,
+        reply_markup: new InlineKeyboard()
+          .url("Source", "https://github.com/darvesh/thewatbot")
+          .url("Buy me a coffee", "https://buymeacoffee.com/darvesh"),
+      }
+    )
+  )
 );
 
 bot.inlineQuery(/^[\w\s]+$/, async (ctx) => {
